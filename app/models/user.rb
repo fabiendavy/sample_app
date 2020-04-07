d7aa3cda8ffc2fa7464eb879f5bced8f682072df
Fabien Davy <davyfabien@gmail.com>
2020-04-07 11:58:11 +0200

    create micropost and display on the profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   before_save   :downcase_email
   before_create :create_activation_digest
 
+  has_many :microposts, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, 
                     length: { maximum: 255 }, 
